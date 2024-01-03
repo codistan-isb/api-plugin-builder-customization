7172b2398a7042434b9a582f4b3e7739cad54ae9
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2024-01-03 16:56:02 +0500

    New updation

--- src/resolvers/Query/customizationKeyValues.js
+++ src/resolvers/Query/customizationKeyValues.js
@@ -1,20 +1,4 @@
-export default async function customizationKeyValues(_, args, context, info) {
-  const { customizationKeyValueArray, selectedDependentCustomizationKeyValue } =
-    args;
-  //   console.log("IN main function ", custId);
 
-  let customizationReports = await context.queries.customizationKeyValues(
-    context,
-    {
-      customizationKeyValueArray,
-      selectedDependentCustomizationKeyValue,
-    }
-  );
-
-  console.log("before return");
-
-  return customizationReports;
-}
 
 export default async function customizationKeyValues(_, args, context, info) {
   const { customizationKeyValueArray, selectedDependentCustomizationKeyValue } =
