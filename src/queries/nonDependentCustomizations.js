3c1850e47cc983fffa68d2be785e238556d76544
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2024-01-16 12:04:24 +0500

    updated: nonDependentCustomizations api

--- src/queries/nonDependentCustomizations.js
+++ src/queries/nonDependentCustomizations.js
@@ -6,20 +6,20 @@ export default async function nonDependentCustomizations(args, context) {
   let { variantId } = args;
   // console.log("variantId", variantId);
   // console.log(
-  //   " encodeProductOpaqueId(variantId)",
+  //   " encodeProductOpaqueId (variantId) :",
   //   decodeProductOpaqueId(variantId)
   // );
-  let totalCountValue =  await Customizations.countDocuments({
+  let totalCountValue = await Customizations.countDocuments({
     productId: decodeProductOpaqueId(variantId),
   });
-  // console.log("totalCountValue",totalCountValue);
-  let nonDependentCustomizations = await Customizations.find({
+  // console.log("totalCountValue", totalCountValue);
+  let nonDependentCustomizationsResponse = await Customizations.find({
     productId: decodeProductOpaqueId(variantId),
   }).toArray();
-  // console.log("nonDependentCustomizations", nonDependentCustomizations);
+  // console.log("nonDependentCustomizations ", nonDependentCustomizationsResponse);
   // return nonDependentCustomizations;
   return {
     totalCount: totalCountValue,
-    nonDependentCustomizations: nonDependentCustomizations,
+    data: nonDependentCustomizationsResponse,
   };
 }
